fd9ce6bb9a10bb6ccfc2d7bdf80a83eb9ab0e75a
tetsuhiro.hayama <tetsuhiro.hayama@gmail.com>
2020-01-27 11:19:27 +0900

    dicWeb #1

---
+++ sample_pg.rb
@@ -0,0 +1,32 @@
+# pgライブラリを使用する
+# pgライブラリを使用する
+require 'pg'
+
+# PG::connect(dbname: "goya")により、rubyからgoyaDBに接続し
+# 接続したという情報をconnectionという名前の変数に格納する
+connection = PG::connect(dbname: "goya")
+connection.internal_encoding = "UTF-8"
+begin
+  # connection変数を使いPostgreSQLを操作する
+  # .execで、goyaDBに"select weight, give_for from crops;"
+  # のSQLの命令文を直接実行し、その結果をresult変数に格納する
+  result = connection.exec("SELECT * FROM crops WHERE give_for <> '自家消費';")
+
+  # 取り出した各行を処理する
+  result.each do |record|
+      # 各行を取り出し、putsでターミナル上に出力する
+      puts "ゴーヤの長さ：#{record["length"]}　重さ：#{record["weight"]} 質：#{record["quality"]} 売った相手:#{record["give_for"]} 売買日:#{record["date"]}"
+  end
+
+  result2 = connection.exec("SELECT * FROM crops WHERE quality = false;")
+
+  # 取り出した各行を処理する
+  result2.each do |record|
+      # 各行を取り出し、putsでターミナル上に出力する
+      puts "ゴーヤの長さ：#{record["length"]}　重さ：#{record["weight"]} 質：#{record["quality"]} 売った相手:#{record["give_for"]} 売買日:#{record["date"]}"
+  end
+ensure
+  # 何かしらのエラーが発生した場合、
+  # .finishでデータベースへのコネクションを切断する
+  connection.finish
+end
---
+++ test.rb
@@ -0,0 +1,19 @@
+require 'webrick'
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+
+server.mount('/dicWeb', WEBrick::HTTPServlet::ERBHandler, 'dicWeb.html.erb')
+
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+
+# この一行を追記
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+server.start
